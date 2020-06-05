0795f543c0e8c05c062543fa102601798f554e39
MohamadNezam <56471354+MohamadNezam@users.noreply.github.com>
2020-06-05 15:21:09 +0200

    Create eventsDetailes.js

---
+++ statics/js/eventsDetailes.js
@@ -0,0 +1,53 @@
+"use strict";
+
+$(document).ready(() => {
+    
+  
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const eventID = urlParams.get('eventID');
+    console.log(eventID);
+    if(eventID != null)
+    fetch(parseInt(eventID));
+});
+
+                   
+	
+
+function drawEventDetailes(data) {
+ 
+    document.getElementById("eventDay").innerHTML = data[0].event_date;
+     
+    document.getElementById("eventtitle").innerHTML = data[0].title;
+    document.getElementById("eventdescription").innerHTML = data[0].description;
+    document.getElementById("eventstart_time").innerHTML = data[0].start_time;
+    document.getElementById("eventend_time").innerHTML = data[0].end_time;  
+    
+     $('#eventimage').css("background-image", "url(" + data[0].image + ")");
+    $('#iFramelocation').attr('src', data[0].location);
+}
+
+
+function fetch(eventId) {
+    jQuery.ajax({
+        url: 'https://hyp-project.herokuapp.com/api/events/'+eventId ,
+        type: 'GET',
+        dataType: 'json',
+        Origin: "https://hyp-project.herokuapp.com",
+        success: (data) => {
+            console.log('ajax success');
+              
+             drawEventDetailes(data);
+             
+        },
+        error: ()=>{
+            notifyerror("error");
+        }
+    });
+}
+
+ 
+
+
+  
+ 
