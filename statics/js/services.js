a53d6aff046c2d394e4031e5cf64ffb60f1dc315
MohamadNezam <56471354+MohamadNezam@users.noreply.github.com>
2020-06-05 16:58:46 +0200

    Update services.js

--- statics/js/services.js
+++ statics/js/services.js
@@ -8,7 +8,15 @@ $(document).ready(() => {
     const urlParams = new URLSearchParams(queryString);
     const serviceID = urlParams.get('CategoryId');
     console.log(serviceID);
-    if(serviceID != null)
+     if(serviceID != null)
+    {
+    	fetchServices('https://hyp-project.herokuapp.com/api/categories/'+serviceID+'/services');
+       document.getElementById("validationCustom03").value=serviceID;
+
+  	var text=$("#validationCustom03 option:selected").text();  
+ 	 document.getElementById("servicesSelector").innerHTML =  text +  " Services";
+    }
+
     fetchServices('https://hyp-project.herokuapp.com/api/categories/'+serviceID+'/services');
     else{
         fetchServices('https://hyp-project.herokuapp.com/api/services');
@@ -42,10 +50,10 @@ function drawServices(data) {
         //data.length
              s = s    +'<div class="col-md-4">'
                       +'<div class="card  mb-4 shadow-sm sechover">'				
-                      +'<a href="#"> <img class="img-fluid" src="'+data[i].image+'" alt="'+data[i].image+'"></a>'
+                      +'<a href="/serviceDetailes.html?serviceID='+data[i].id_activity+'"> <img class="img-fluid" src="'+data[i].image+'" alt="'+data[i].image+'"></a>'
                       +' <div class="card-body">'
                       +'  <h5><b>'+data[i].title+'</b></h5>'
-                      +' <h6 class="card-text">'+data[i].description.substring(0,40)+'...'+' <u class="linkcolor"> <a href="#">more</a> </u></h6>'
+                      +' <h6 class="card-text">'+data[i].description.substring(0,40)+'...'+' <u class="linkcolor"> <a href="/serviceDetailes.html?serviceID='+data[i].id_activity+'">more</a> </u></h6>'
                       +'<div class="d-flex justify-content-between align-items-center">  '
                       +' <small class="text-muted">'+data[i].service_day+'</small>'
                       +'</div></div></div></div>'
