7dddfb34ba0fc0ec835b45e30e4761198decc160
MohamadNezam <56471354+MohamadNezam@users.noreply.github.com>
2020-06-05 16:54:10 +0200

    Update services.js

--- statics/js/services.js
+++ statics/js/services.js
@@ -1,37 +1,54 @@
 "use strict";
 
 $(document).ready(() => {
-
+    
+    
+     
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const serviceID = urlParams.get('CategoryId');
     console.log(serviceID);
     if(serviceID != null)
-    {
-    	fetchServices('https://hyp-project.herokuapp.com/api/categories/'+serviceID+'/services');
-       document.getElementById("validationCustom03").value=serviceID;
- 
-  	var text=$("#validationCustom03 option:selected").text();  
- 	 document.getElementById("servicesSelector").innerHTML =  text +  " Services";
-    }
-    
+    fetchServices('https://hyp-project.herokuapp.com/api/categories/'+serviceID+'/services');
     else{
         fetchServices('https://hyp-project.herokuapp.com/api/services');
     }
+  
 });
 
-function drawServices(data) {  
-    var s = '';
-  for (var i = 0; i < data.length; i++) {
+function drawServices(data) {
+    
+    var shortDescription= 'hh';
+    var backgroung='bg-light';
+    /*
+    var id_activity='';
+    var location= '';
+    var title= '';
+    
+    var start_time= '';
+    var end_time= '';
+    var image= '';
+    var service_day= '';
+    var capacity= '';
+    var age= '';
+    var id_category= '';
+    
+    */
        
-         s = s        +'<div class="card col-sm-3  mt-3 ml-4 mb-4 shadow-sm sechover">'				
-                      +'<a href="/serviceDetailes.html?serviceID='+data[i].id_activity+'"> <img class="img-fluid" src="'+data[i].image+'" alt="'+data[i].image+'"></a>'
+   
+         
+
+  for (var i = 0; i < data.length; i++) {
+        //data.length
+             s = s    +'<div class="col-md-4">'
+                      +'<div class="card  mb-4 shadow-sm sechover">'				
+                      +'<a href="#"> <img class="img-fluid" src="'+data[i].image+'" alt="'+data[i].image+'"></a>'
                       +' <div class="card-body">'
                       +'  <h5><b>'+data[i].title+'</b></h5>'
-                      +' <h6 class="card-text">'+data[i].description.substring(0,40)+'...'+' <u class="linkcolor"> <a href="/serviceDetailes.html?serviceID='+data[i].id_activity+'">more</a> </u></h6>'
+                      +' <h6 class="card-text">'+data[i].description.substring(0,40)+'...'+' <u class="linkcolor"> <a href="#">more</a> </u></h6>'
                       +'<div class="d-flex justify-content-between align-items-center">  '
                       +' <small class="text-muted">'+data[i].service_day+'</small>'
-                      +'</div></div>  </div>'
+                      +'</div></div></div></div>'
                             
                                  
 
