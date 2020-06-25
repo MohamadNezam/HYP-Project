2b290f1e7c740047b9c2f14c31bbfbc51b3027c0
MohamadNezam <56471354+MohamadNezam@users.noreply.github.com>
2020-06-25 19:52:29 +0200

    Create persons.js

---
+++ statics/js/persons.js
@@ -0,0 +1,62 @@
+"use strict";
+
+$(document).ready(() => {
+  fetchEvents('https://hyp-project.herokuapp.com/api/volunteers');
+});
+
+function drawEvents(data) {
+ 
+    var s = '';
+ 
+    var backgroung='bg-light';
+ 
+  for (var i = 0; i < data.length; i++) {
+        //data.length
+        
+         s = s        +'<div class="card col-sm-3  mt-3 ml-4 mb-4 shadow-sm sechover">'				
+                      +'<a href="/personDetailes.html?ID='+data[i].id_person+'"> <img class="img-fluid" src="'+data[i].photo+'" alt=""></a>'
+                      +' <div class="card-body">'
+                  //    +'  <h5><b>'+data[i].title+'</b></h5>'
+                      +' <h6 class="card-text">'+data[i].description.substring(0,40)+'...'+' <u class="linkcolor"> <a href="/personDetailes.html?ID='+data[i].id_person+'">more</a> </u></h6>'
+                      +'<div class="d-flex justify-content-between align-items-center">  '
+                      +' <small class="text-muted">volunteering date: '+data[i].volunteer_time+'</small>'
+                      +'</div></div>  </div>'
+                            
+                                 
+
+    }
+     
+    return s;
+}
+
+
+        
+                        
+                                    
+	
+
+
+
+function fetchEvents(ApiUrl) {
+    jQuery.ajax({
+        url: ApiUrl ,
+        type: 'GET',
+        dataType: 'json',
+        Origin: "https://hyp-project.herokuapp.com",
+        success: (data) => {
+            console.log('ajax success');
+            var s = drawEvents(data);
+            
+            $('#persons').html(s);
+        },
+        error: ()=>{
+            notifyerror("error");
+        }
+    });
+}
+
+ 
+
+
+  
+ 
